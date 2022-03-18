77004f8df5dc1af4a86ed2da5ed42116345a2125
Pierce Gearhart <61991125+piercegearhart@users.noreply.github.com>
2022-03-17 20:36:24 -0400

    Easy version

---
+++ src/pages/index.js
@@ -0,0 +1,13 @@
+// React Router
+import { Link } from 'react-router-dom'
+
+const Index = () => {
+    return (
+        <>
+            {/* Your HTML code here */}
+            <Link to='/'>Home</Link>
+        </>
+    )
+}
+
+export default Index
